2b66f691f9a19ff5d4eb48890e8d5054422c75e2
José Manuel Romo <JoseManuel.Romo.96@gmail.com>
2021-08-12 14:11:48 -0500

    ..

--- build_Xcode/examples/CPP/BuildTest/src/CPString.build/Debug/BuildTest.build/Objects-normal/x86_64/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/examples/CPP/BuildTest/src/main.cpp \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/CPString.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/CPString_BuildSettings.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/CPString_Definitions.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/Flash/CPString_Flash.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/Flash/CPString_FlashDefinitions.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/CPString_Class.h
--- build_Xcode/src/CPString.build/Debug/CPString.build/Objects-normal/x86_64/CPString_Flash.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/Flash/CPString_Flash.cpp \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/Flash/CPString_Flash.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/CPString_BuildSettings.h \
-  /Users/josemanuelromoperedo/Dropbox/GitHub/C++/CPString/src/Flash/CPString_FlashDefinitions.h
