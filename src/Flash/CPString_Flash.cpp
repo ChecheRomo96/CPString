b3c503fa1e67863e91382047d6665fba5f7b2228
José <josemanuel.romo.96@gmail.com>
2023-01-25 04:23:26 -0600

    .

--- src/Flash/CPString_Flash.cpp
+++ src/Flash/CPString_Flash.cpp
@@ -4,10 +4,10 @@ const char* CrossPlatformString::Flash::CopyBaseChars(char* buff, uint8_t base, 
 {
 	for(uint8_t i = 0; i < base; i++)
 	{
-		#if defined(PSOC_CREATOR) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(__APPLE__) || defined(linux)
-	        buff[i] = CrossPlatformString::Flash::NumericBaseChars[LetterCase][i];
-	    #elif defined(ARDUINO)
+		#if defined(__AVR__)
 		    buff[i] = pgm_read_byte(&CrossPlatformString::Flash::NumericBaseChars[LetterCase][i]);
+		#else
+		    buff[i] = CrossPlatformString::Flash::NumericBaseChars[LetterCase][i];
 		#endif
 	}
     return buff;
